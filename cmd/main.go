4ebe4ec5277b5fb0d05835f1a8746ca8a7b1ecc1
Jche143 <1246027231@qq.com>
2024-03-07 20:54:52 +0800

    fix(connect main and init)

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"Target/conf"
+	"Target/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db := conf.InitDB()
+
+	defer db.Close()
+
 	r := gin.Default()
+
+	routes.CollectRoutes(r)
 
 	r.Run() // 启动服务，并监听 8080 端口
 }
